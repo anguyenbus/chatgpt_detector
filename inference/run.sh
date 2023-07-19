6464c192620f32a7657e745927a3b5c8f5b8acdd
an ng <anguyenbus@gmail.com>
2023-07-19 10:27:58 +1000

    [main] add inference

--- inference/run.sh
+++ inference/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# inference
+
+# Check if the "latest" file exists locally
+if [ -f "$local_directory$latest_file" ]; then
+    echo "Latest file already exists locally. Skipping download."
+else
+    echo "Latest file does not exist locally. Downloading..."
+    aws s3 sync s3://chatgptdetector/chatgptdetector/latest/model/ latest
+fi
+
 python3 inference.py
